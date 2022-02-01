e74caedee2d5994654b136f7034ea8e9446291f8
LozinskyiValerii <argosumy@gmail.com>
2022-02-01 14:49:27 +0200

    Initial commit

---
+++ src/test/java/spdu2022/java/project/beutysalon/BeutySalonApplicationTests.java
@@ -0,0 +1,13 @@
+package spdu2022.java.project.beutysalon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeutySalonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
