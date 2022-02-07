8d5922725986ed5149d631129a50e055f8357c77
LozinskyiValerii <argosumy@gmail.com>
2022-02-07 12:35:10 +0200

    Update SHEMA_DB.pdf and update script create table

--- src/main/resources/db/migration/V20220201220135__create-tables.sql
+++ src/main/resources/db/migration/V20220201220135__create-tables.sql
@@ -4,8 +4,8 @@ CREATE TABLE IF NOT EXISTS users
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     phone VARCHAR(15) NOT NULL UNIQUE,
-    location_id INT,
-    link_foto VARCHAR (300)
+    city VARCHAR(50)
+
 );
 
 CREATE TABLE IF NOT EXISTS user_roles
@@ -15,59 +15,41 @@ CREATE TABLE IF NOT EXISTS user_roles
     name_role VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS locations
-(
-    id SERIAL PRIMARY KEY NOT NULL,
-    city VARCHAR (50) NOT NULL,
-    country_code INT NOT NULL UNIQUE
-);
-
 CREATE TABLE IF NOT EXISTS salons
 (
     id SERIAL PRIMARY KEY NOT NULL,
     salon_name VARCHAR(100) NOT NULL,
     phone VARCHAR(15) NOT NULL UNIQUE,
-    location_id INT
+    city VARCHAR(50)
 );
 
 CREATE TABLE IF NOT EXISTS staff
 (
-    salon_id INT NOT NULL,
-    user_id INT NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS services
-(
     id SERIAL PRIMARY KEY NOT NULL,
-    service_name VARCHAR(100) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS salon_services
-(
-    service_id INT NOT NULL,
     salon_id INT NOT NULL,
-    cost int
-);
-
-CREATE TABLE IF NOT EXISTS salon_services
-(
-    service_id INT NOT NULL,
-    salon_id INT NOT NULL,
-    cost int
-);
-
-CREATE TABLE IF NOT EXISTS staff_services
-(
-    service_id INT NOT NULL,
     user_id INT NOT NULL,
-    service_duration TIMESTAMP
+    staff_foto VARCHAR(300)
 );
 
-CREATE TABLE IF NOT EXISTS service_working_mode
+CREATE TABLE IF NOT EXISTS salon_working_mode
+(
+    salon_id INT NOT NULL,
+    start_working TIMESTAMP NOT NULL,
+    finish_working TIMESTAMP NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS staff_working_mode
 (
     staff_id INT NOT NULL,
-    customer_id INT NOT NULL,
-    service_id INT NOT NULL,
+    working_day DATE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS log_book_services
+(
+    id SERIAL PRIMARY KEY NOT NULL,
+    staff_id INT NOT NULL,
+    user_id INT NOT NULL,
     start_service TIMESTAMP NOT NULL,
     finish_service TIMESTAMP NOT NULL
 );
+
