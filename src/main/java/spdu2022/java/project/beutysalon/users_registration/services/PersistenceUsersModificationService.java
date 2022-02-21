e890218de314ddf99211a88a4380bece053ed634
LozinskyiValerii <argosumy@gmail.com>
2022-02-21 20:01:48 +0200

    implemented PersistenceUsersModificationService

---
+++ src/main/java/spdu2022/java/project/beutysalon/users_registration/services/PersistenceUsersModificationService.java
@@ -0,0 +1,31 @@
+package spdu2022.java.project.beutysalon.users_registration.services;
+
+import org.springframework.stereotype.Service;
+import spdu2022.java.project.beutysalon.entities.User;
+import spdu2022.java.project.beutysalon.users_registration.persistence.repositories.UsersRepository;
+
+import java.sql.SQLException;
+
+@Service
+public class PersistenceUsersModificationService implements UsersModificationService {
+    private final UsersRepository usersRepository;
+
+    public PersistenceUsersModificationService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    @Override
+    public User createNewUser(User user) throws SQLException {
+        return usersRepository.insertNewUser(user);
+    }
+
+    @Override
+    public boolean deleteUserById(long id) {
+        return false;
+    }
+
+    @Override
+    public User updateUserById(long id) {
+        return null;
+    }
+}
