f7a77d936f4a3807e60582dd7033cc48f7ca33e2
LozinskyiValerii <argosumy@gmail.com>
2022-03-02 14:45:55 +0200

    result set extractor for entities

---
+++ src/main/java/spdu2022/java/project/beutysalon/users_registration/persistence/mappers/UsersResultSetExtractor.java
@@ -0,0 +1,24 @@
+package spdu2022.java.project.beutysalon.users_registration.persistence.mappers;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import spdu2022.java.project.beutysalon.entities.User;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UsersResultSetExtractor implements ResultSetExtractor<User> {
+    @Override
+    public User extractData(ResultSet rs) throws SQLException, DataAccessException {
+        if(rs.next()) {
+            User user = new User();
+            user.setId(rs.getLong("id"));
+            user.setFirstName(rs.getString("first_name"));
+            user.setLastName(rs.getString("last_name"));
+            user.setPhone(rs.getString("phone"));
+            user.setCity(rs.getString("city"));
+            return user;
+        }
+        return new User();
+    }
+}
