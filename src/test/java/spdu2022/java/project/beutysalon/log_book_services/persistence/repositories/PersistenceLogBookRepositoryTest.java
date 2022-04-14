0362e277080d9feb3a786e9eb4830c3e035a1a7f
LozinskyiValerii <argosumy@gmail.com>
2022-04-14 17:24:10 +0300

    implementation test container

--- src/test/java/spdu2022/java/project/beutysalon/log_book_services/persistence/repositories/PersistenceLogBookRepositoryTest.java
+++ src/test/java/spdu2022/java/project/beutysalon/log_book_services/persistence/repositories/PersistenceLogBookRepositoryTest.java
@@ -1,15 +1,16 @@
 package spdu2022.java.project.beutysalon.log_book_services.persistence.repositories;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import spdu2022.java.project.beutysalon.entities.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import spdu2022.java.project.beutysalon.entities.BookedService;
+import spdu2022.java.project.beutysalon.entities.Salon;
+import spdu2022.java.project.beutysalon.entities.WorkingDay;
+import spdu2022.java.project.beutysalon.entities.WorkingDayOfWeek;
 
-import javax.sql.DataSource;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -18,39 +19,32 @@ import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@Testcontainers
+@ActiveProfiles("test-containers")
 class PersistenceLogBookRepositoryTest {
-    private static PersistenceLogBookRepository repository;
-    private static NamedParameterJdbcTemplate namedJdbcTemplate;
-
-    @BeforeAll
-    static void init() {
-        DataSource dataSource = new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2)
-                .addScript("classpath:test-data.sql")
-                .build();
-        namedJdbcTemplate = new NamedParameterJdbcTemplate(new JdbcTemplate(dataSource));
-        repository = new PersistenceLogBookRepository(namedJdbcTemplate);
-        System.out.println(repository);
-    }
+    @Autowired
+    private PersistenceLogBookRepository repository;
 
     @Test
-    @DisplayName("Method must return List of table's row (booking services for the city by date period.)")
+    @DisplayName("Method must return List of booking services for the city by date period.")
     void getLogServiceByCityAndPeriod() {
         List<BookedService> res = repository.getLogServiceByCityAndPeriod("Sumy", LocalDate.parse("2023-05-01"), LocalDate.parse("2023-05-01"));
-        assertEquals(3, res.size());
-        assertTrue(res.stream().anyMatch(x -> x.getSalonId() == 2L));
+        assertEquals(2, res.size());
+        assertTrue(res.stream().anyMatch(x -> x.getSalonId() == 1L));
 
         res = repository.getLogServiceByCityAndPeriod("Kiev", LocalDate.parse("2023-05-01"), LocalDate.parse("2023-05-01"));
         assertEquals(0, res.size());
     }
 
     @Test
-    @DisplayName("Method must return List of table's row.(booking services for the salonId by date period.)")
+    @DisplayName("Method must return List of booking services for the salonId by date period.")
     void getLogServiceBySalonIdAndPeriod() {
         List<BookedService> res = repository.getLogServiceBySalonIdAndPeriod(1, LocalDate.parse("2023-05-01"), LocalDate.parse("2023-05-02"));
         assertEquals(2, res.size());
 
-        res = repository.getLogServiceBySalonIdAndPeriod(1, LocalDate.parse("2023-05-01"), LocalDate.parse("2023-05-05"));
-        assertEquals(3, res.size());
+        res = repository.getLogServiceBySalonIdAndPeriod(2, LocalDate.parse("2023-05-01"), LocalDate.parse("2023-05-01"));
+        assertEquals(0, res.size());
     }
 
     @Test
@@ -62,15 +56,15 @@ class PersistenceLogBookRepositoryTest {
         assertTrue(res.containsKey(2L));
 
         res = repository.getUniqWorkingMode(2, LocalDate.parse("2023-05-01"));
-        assertTrue(res.containsKey(3L));
+        assertEquals(0, res.size());
     }
 
     @Test
     @DisplayName("Method must return working mode for salon by day of week")
     void findWorkingWeekPeriodBySalonIdAndDayOfWeek() {
-        WorkingDayOfWeek res = (WorkingDayOfWeek)repository.findWorkingWeekPeriodBySalonIdAndDayOfWeek(1, DayOfWeek.FRIDAY.name());
+        WorkingDayOfWeek res = repository.findWorkingWeekPeriodBySalonIdAndDayOfWeek(1, DayOfWeek.FRIDAY.name());
         assertEquals(DayOfWeek.FRIDAY, res.getDayOfWeek());
-        res = (WorkingDayOfWeek)repository.findWorkingWeekPeriodBySalonIdAndDayOfWeek(3, DayOfWeek.FRIDAY.name());
+        res = repository.findWorkingWeekPeriodBySalonIdAndDayOfWeek(3, DayOfWeek.FRIDAY.name());
         assertNull(res);
     }
 
