6e82b79c3f787132213b8eebb7246579c0f008b7
LozinskyiValerii <argosumy@gmail.com>
2022-05-03 19:44:55 +0300

    staff storage controller test

--- src/main/java/spdu2022/java/project/beutysalon/staff_registration/controllers/StaffFileStorageController.java
+++ src/main/java/spdu2022/java/project/beutysalon/staff_registration/controllers/StaffFileStorageController.java
@@ -2,12 +2,16 @@ package spdu2022.java.project.beutysalon.staff_registration.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import spdu2022.java.project.beutysalon.staff_registration.services.StaffFileStorage;
 
+import javax.validation.constraints.Min;
+
 @RestController
 @RequestMapping("api/v1/staff/{staffId}/file")
+@Validated
 public class StaffFileStorageController {
     private final StaffFileStorage staffFileStorage;
 
@@ -16,12 +20,13 @@ public class StaffFileStorageController {
     }
 
     @PostMapping
-    public ResponseEntity<String> saveAvatar(@RequestParam("file") MultipartFile file, @PathVariable("staffId") long staffId) {
-        return new ResponseEntity<>(staffFileStorage.saveAvatarOfStaff(staffId, file), HttpStatus.OK);
+    public ResponseEntity<String> saveAvatar(@RequestParam("file") MultipartFile file, @Min(value = 1, message = "id must be  > 0") @PathVariable("staffId") long staffId) {
+        String link = staffFileStorage.saveAvatarOfStaff(staffId, file);
+        return new ResponseEntity<>(link, HttpStatus.CREATED);
     }
 
     @DeleteMapping
-    public ResponseEntity<String> deleteAvatar(@PathVariable("staffId") long staffId) {
+    public ResponseEntity<String> deleteAvatar(@PathVariable("staffId") @Min(value = 1, message = "id must be  > 0")long staffId) {
         staffFileStorage.deleteAvatarOfStaff(staffId);
         return new ResponseEntity<>(HttpStatus.OK);
     }
---
+++ src/test/java/spdu2022/java/project/beutysalon/staff_registration/controllers/StaffFileStorageControllerTest.java
@@ -0,0 +1,63 @@
+package spdu2022.java.project.beutysalon.staff_registration.controllers;
+
+import com.amazonaws.util.IOUtils;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+import spdu2022.java.project.beutysalon.staff_registration.services.StaffFileStorage;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = StaffFileStorageController.class)
+class StaffFileStorageControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    StaffFileStorage staffFileStorage;
+
+    @Test
+    void saveAvatar() throws Exception {
+        mockMvc.perform(multipart("/api/v1/staff/{staffId}/file", 1)
+                .file(new MockMultipartFile("file", getMultipartFileBytes())))
+                .andExpect(status().is(201));
+    }
+
+    @Test
+    @DisplayName("Response has status 'Bad Request' if staffId < 1")
+    void saveAvatarStaffIdNotValid() throws Exception {
+        mockMvc.perform(multipart("/api/v1/staff/{staffId}/file", 0)
+                .file(new MockMultipartFile("file", getMultipartFileBytes())))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void deleteAvatar() throws Exception {
+        mockMvc.perform(delete("/api/v1/staff/{staffId}/file", 1))
+                .andExpect(status().is(200));
+    }
+
+    @Test
+    @DisplayName("Response has status 'Bad Request' if staffId < 1")
+    void deleteAvatarStaffIdNotValid() throws Exception {
+        mockMvc.perform(delete("/api/v1/staff/{staffId}/file", 0))
+                .andExpect(status().isBadRequest());
+    }
+
+    private byte[] getMultipartFileBytes() throws IOException {
+        File file = new File("src/test/resources/file/avatar.jpg");
+        FileInputStream input = new FileInputStream(file);
+        return new MockMultipartFile("file",
+                file.getName(), "image/jpeg", IOUtils.toByteArray(input)).getBytes();
+    }
+}
